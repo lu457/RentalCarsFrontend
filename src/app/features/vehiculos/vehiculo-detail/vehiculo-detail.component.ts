86efbae16df22acd62e2365258b767c509261353
Moreno <mauro.lider.123@gmail.com>
2025-03-07 01:28:50 -0500

    Primer commit: Proyecto Angular creado

---
+++ src/app/features/vehiculos/vehiculo-detail/vehiculo-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehiculo-detail',
+  imports: [],
+  templateUrl: './vehiculo-detail.component.html',
+  styleUrl: './vehiculo-detail.component.css'
+})
+export class VehiculoDetailComponent {
+
+}
